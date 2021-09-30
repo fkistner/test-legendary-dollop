b0611495e3ac86d9ce4fa819a702db352f632d6a
GitHub Action <action@github.com>
2021-09-30 17:37:32 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/fkistner/testlegendarydollop/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.fkistner.testlegendarydollop.services
+
+import com.github.fkistner.testlegendarydollop.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/fkistner/testlegendarydollop/services/MyProjectService.kt
+++ src/main/kotlin/com/github/fkistner/testlegendarydollop/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.fkistner.testlegendarydollop.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fkistner.testlegendarydollop.MyBundle
 
 class MyProjectService(project: Project) {
 
